ce07a7a0d45b340e27f81f575497c2ffa39df4bd
Umubyeyi-Marie <mariebienvenueu@gmail.com>
2024-01-02 16:07:55 +0200

    my first commit

--- Desktop/tutorial 8hr/app.js
+++ Desktop/tutorial 8hr/app.js
@@ -4,8 +4,8 @@
 
 const_ = require('lodash');
 
-const items =[1[2,[3,[4,[5]]]]]
+const items1 =[1[2,[3,[4,[5]]]]]
 
-const newItems = _.flattenDeep(items);
+const newItems1 = _.flattenDeep(items1);
 
-console.log(newItems);
+console.log(newItems1);
